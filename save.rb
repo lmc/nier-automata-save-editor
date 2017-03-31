81482de20cb89ddfd581db7fb41d278a9ff636bd
Luke Mcildoon <luke@twofiftyfive.net>
2017-04-01 10:57:57 +1100

    more chip docs

--- save.rb
+++ save.rb
@@ -1,6 +1,7 @@
 
 CHIP_OFFSET = 206012
 CHIP_SIZE = 48
+CHIP_MAX = 300
 
 CHIP_NIL = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]
 
@@ -31,6 +32,30 @@ CATEGORIES = {
   41 => "HUD: Enemy Data",
   40 => "HUD: Sound Waves",
   39 => "HUD: HP Gauge",
+  47 => "Auto-Collect Items",
+  45 => "Continuous Combo",
+  46 => "Bullet Detonation",
+  44 => "Evasive System",
+  38 => "Death Rattle",
+   3 => "Critical Up",
+  12 => "Auto Heal",
+  17 => "Shock Wave",
+  19 => "Damage Absorb",
+  21 => "Reset",
+  10 => "Offensive Heal",
+  11 => "Deadly Heal",
+  13 => "Evade Range Up",
+  14 => "Moving Speed Up",
+  18 => "Last Stand",
+  20 => "Vengeance",
+  22 => "Overclock",
+  25 => "Taunt Up",
+  27 => "Auto-Use Item",
+   2 => "Down Attack Up",
+  23 => "Resilience",
+  24 => "Counter",
+  26 => "Charge Attack",
+  31 => "Combust",
 }
 
 CHIP_IDS = {
@@ -78,16 +103,47 @@ CHIP_IDS = {
   3336 => "HUD: Sound Waves",
   3335 => "HUD: HP Gauge",
 
+  3342 => "Auto-Collect Items",
+
+  3340 => "Continuous Combo",
+
+  3341 => "Bullet Detonation",
+
+  3339 => "Evasive System",
+
+  3334 => "Death Rattle",
+
+  3019 => "Critical Up",
+  3100 => "Auto Heal",
+  3145 => "Shock Wave",
+  3163 => "Damage Absorb",
+  3181 => "Reset",
+
+  3082 => "Offensive Heal",
+  3091 => "Deadly Heal",
+  3109 => "Evade Range Up",
+  3118 => "Moving Speed Up",
+  3154 => "Last Stand",
+  3172 => "Vengeance",
+  3190 => "Overclock",
+  3226 => "Taunt Up",
+  3244 => "Auto-Use Item",
+  3010 => "Down Attack Up",
+  3199 => "Resilience",
+  3217 => "Counter",
+  3235 => "Charge Attack",
+  3298 => "Combust",
+
 }
 
 # File.open("SlotData_nil.dat","rb") do |f|
-File.open("SlotData_1.dat","rb+") do |f|
+File.open("SlotData_overclock_+1_5s.dat","rb+") do |f|
   f.seek(CHIP_OFFSET)
-  64.times do |i|
+  CHIP_MAX.times do |i|
     bytes = f.read(CHIP_SIZE)
 
     data = bytes.unpack("l<*")
-    puts "#{i}: #{data.inspect}"
+    # puts "#{i}: #{data.inspect}"
 
     id        = data[ 0 ]
     chip_id   = data[ 1 ]
@@ -95,6 +151,7 @@ File.open("SlotData_1.dat","rb+") do |f|
     _level    = data[ 3 ]
     slots     = data[ 4 ]
 
+    puts "#{i}: #{chip_id} (#{CHIP_IDS[chip_id] || "?"}), category: #{_category}, level: #{_level}, slots: #{slots}"
 
     # if i == 4
     #   payload = [112,3076,9,5,10,-1,-1,-1,-1,-1,-1,0]
