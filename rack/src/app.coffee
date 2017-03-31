b4a1eab65c9243ff0daec5905d0898284671a857
Luke Mcildoon <luke@twofiftyfive.net>
2017-03-31 22:34:20 +1100

    more

--- rack/src/app.coffee
+++ rack/src/app.coffee
@@ -2,10 +2,104 @@ class @App
   constructor: () ->
     document.getElementById('file').addEventListener('change', @file_change, false)
     @output = document.getElementById('output')
+    @tbody = document.getElementById('tbody')
 
   CHIPS_OFFSET: 206012
   CHIPS_SIZE: 48
-  CHIPS_COUNT: 300
+  # CHIPS_COUNT: 300
+  CHIPS_COUNT: 64
+  CHIP_NIL: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]
+
+  CATEGORIES: {
+    42 : "System",
+    9  : "Max HP Up",
+    29 : "Hijack Boost",
+    30 : "Stun",
+    7  : "Ranged Defense",
+    6  : "Melee Defense",
+    4  : "Ranged Attack Up",
+    1  : "Weapon Attack Up",
+    63 : "Auto Weapon Switch",
+    62 : "Auto Program",
+    61 : "Auto Evade",
+    60 : "Auto Fire",
+    59 : "Auto Attack",
+    35 : "Item Scan",
+    58 : "HUD: Fishing Spots",
+    55 : "HUD: Control",
+    54 : "HUD: Objectives",
+    53 : "HUD: Damage Values",
+    52 : "HUD: Save Points",
+    51 : "HUD: EXP Gauge",
+    50 : "HUD: Mini-Map",
+    49 : "HUD: Text Log",
+    48 : "HUD: Skill Gauge",
+    41 : "HUD: Enemy Data",
+    40 : "HUD: Sound Waves",
+    39 : "HUD: HP Gauge",
+    47 : "Auto-Collect Items",
+    45 : "Continuous Combo",
+    46 : "Bullet Detonation",
+    44 : "Evasive System",
+    38 : "Death Rattle",
+  }
+
+  CHIP_IDS: {
+    3338 : "OS Chip",
+
+    3073 : "Max HP Up",
+    3074 : "Max HP Up +1",
+    3075 : "Max HP Up +2",
+    3076 : "Max HP Up +3",
+
+    3289 : "Stun",
+    3290 : "Stun +1",
+    3291 : "Stun +2",
+
+    3262 : "Hijack Boost",
+    3263 : "Hijack Boost +1",
+    3264 : "Hijack Boost +2",
+
+    3055 : "Ranged Defense",
+    3046 : "Melee Defense",
+    3047 : "Melee Defense +1",
+
+    3028 : "Ranged Attack Up",
+
+    3001 : "Weapon Attack Up",
+
+    3358 : "Auto Weapon Switch",
+    3357 : "Auto Program",
+    3356 : "Auto Evade",
+    3355 : "Auto Fire",
+    3354 : "Auto Attack",
+
+    3208 : "Item Scan",
+
+    3353 : "HUD: Fishing Spots",
+    3350 : "HUD: Control",
+    3349 : "HUD: Objectives",
+    3348 : "HUD: Damage Values",
+    3347 : "HUD: Save Points",
+    3346 : "HUD: EXP Gauge",
+    3345 : "HUD: Mini-Map",
+    3344 : "HUD: Text Log",
+    3343 : "HUD: Skill Gauge",
+    3337 : "HUD: Enemy Data",
+    3336 : "HUD: Sound Waves",
+    3335 : "HUD: HP Gauge",
+
+    3342 : "Auto-Collect Items",
+
+    3340 : "Continuous Combo",
+
+    3341 : "Bullet Detonation",
+
+    3339 : "Evasive System",
+
+    3334 : "Death Rattle",
+
+  }
 
   file_change: (evt) =>
     files = evt.target.files
@@ -28,8 +122,29 @@ class @App
         j += 4
       chips.push(ints)
 
-    for ints in chips
-      @output.innerText += "[#{ints.join(', ')}]\n"
+    for ints,idx in chips
+      @tbody.innerHTML += @row_html(ints,idx)
+
+  row_html: (chip,index) =>
+    """
+      <tr id="chip_#{index}">
+        <td>
+          <input name="chip[#{index}][id]" value="#{chip[0]}">
+        </td>
+        <td>
+          <select name="chip[#{index}][chip]" class="chip">#{@chip_options_html(chip)}</select>
+        </td>
+        <td>
+          <input name="chip[#{index}][slots]" value="#{chip[4]}">
+        </td>
+      </tr>
+    """
+
+  chip_options_html: (chip) =>
+    html = "<option value>-</option>"
+    for chip_id,name of @CHIP_IDS
+      html += "<option value='#{chip_id}' #{if parseInt(chip_id) == parseInt(chip[1]) then ' selected' else ''}>#{name}</option>\n"
+    html
 
 window.onload = ->
   window.app = new App
--- rack/src/app.js
+++ rack/src/app.js
@@ -4,18 +4,101 @@
   this.App = (function() {
 
     function App() {
+      this.chip_options_html = __bind(this.chip_options_html, this);
+
+      this.row_html = __bind(this.row_html, this);
+
       this.reader_onload = __bind(this.reader_onload, this);
 
       this.file_change = __bind(this.file_change, this);
       document.getElementById('file').addEventListener('change', this.file_change, false);
       this.output = document.getElementById('output');
+      this.tbody = document.getElementById('tbody');
     }
 
     App.prototype.CHIPS_OFFSET = 206012;
 
     App.prototype.CHIPS_SIZE = 48;
 
-    App.prototype.CHIPS_COUNT = 300;
+    App.prototype.CHIPS_COUNT = 64;
+
+    App.prototype.CHIP_NIL = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0];
+
+    App.prototype.CATEGORIES = {
+      42: "System",
+      9: "Max HP Up",
+      29: "Hijack Boost",
+      30: "Stun",
+      7: "Ranged Defense",
+      6: "Melee Defense",
+      4: "Ranged Attack Up",
+      1: "Weapon Attack Up",
+      63: "Auto Weapon Switch",
+      62: "Auto Program",
+      61: "Auto Evade",
+      60: "Auto Fire",
+      59: "Auto Attack",
+      35: "Item Scan",
+      58: "HUD: Fishing Spots",
+      55: "HUD: Control",
+      54: "HUD: Objectives",
+      53: "HUD: Damage Values",
+      52: "HUD: Save Points",
+      51: "HUD: EXP Gauge",
+      50: "HUD: Mini-Map",
+      49: "HUD: Text Log",
+      48: "HUD: Skill Gauge",
+      41: "HUD: Enemy Data",
+      40: "HUD: Sound Waves",
+      39: "HUD: HP Gauge",
+      47: "Auto-Collect Items",
+      45: "Continuous Combo",
+      46: "Bullet Detonation",
+      44: "Evasive System",
+      38: "Death Rattle"
+    };
+
+    App.prototype.CHIP_IDS = {
+      3338: "OS Chip",
+      3073: "Max HP Up",
+      3074: "Max HP Up +1",
+      3075: "Max HP Up +2",
+      3076: "Max HP Up +3",
+      3289: "Stun",
+      3290: "Stun +1",
+      3291: "Stun +2",
+      3262: "Hijack Boost",
+      3263: "Hijack Boost +1",
+      3264: "Hijack Boost +2",
+      3055: "Ranged Defense",
+      3046: "Melee Defense",
+      3047: "Melee Defense +1",
+      3028: "Ranged Attack Up",
+      3001: "Weapon Attack Up",
+      3358: "Auto Weapon Switch",
+      3357: "Auto Program",
+      3356: "Auto Evade",
+      3355: "Auto Fire",
+      3354: "Auto Attack",
+      3208: "Item Scan",
+      3353: "HUD: Fishing Spots",
+      3350: "HUD: Control",
+      3349: "HUD: Objectives",
+      3348: "HUD: Damage Values",
+      3347: "HUD: Save Points",
+      3346: "HUD: EXP Gauge",
+      3345: "HUD: Mini-Map",
+      3344: "HUD: Text Log",
+      3343: "HUD: Skill Gauge",
+      3337: "HUD: Enemy Data",
+      3336: "HUD: Sound Waves",
+      3335: "HUD: HP Gauge",
+      3342: "Auto-Collect Items",
+      3340: "Continuous Combo",
+      3341: "Bullet Detonation",
+      3339: "Evasive System",
+      3334: "Death Rattle"
+    };
 
     App.prototype.file_change = function(evt) {
       var blob, file, files, reader;
@@ -28,7 +111,7 @@
     };
 
     App.prototype.reader_onload = function(event) {
-      var chips, i, ints, j, view, _i, _j, _len, _ref, _results;
+      var chips, i, idx, ints, j, view, _i, _j, _len, _ref, _results;
       view = new DataView(event.target.result);
       chips = [];
       for (i = _i = 0, _ref = this.CHIPS_COUNT; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
@@ -41,11 +124,26 @@
         chips.push(ints);
       }
       _results = [];
-      for (_j = 0, _len = chips.length; _j < _len; _j++) {
-        ints = chips[_j];
-        _results.push(this.output.innerText += "[" + (ints.join(', ')) + "]\n");
+      for (idx = _j = 0, _len = chips.length; _j < _len; idx = ++_j) {
+        ints = chips[idx];
+        _results.push(this.tbody.innerHTML += this.row_html(ints, idx));
       }
       return _results;
+    };
+
+    App.prototype.row_html = function(chip, index) {
+      return "<tr id=\"chip_" + index + "\">\n  <td>\n    <input name=\"chip[" + index + "][id]\" value=\"" + chip[0] + "\">\n  </td>\n  <td>\n    <select name=\"chip[" + index + "][chip]\" class=\"chip\">" + (this.chip_options_html(chip)) + "</select>\n  </td>\n  <td>\n    <input name=\"chip[" + index + "][slots]\" value=\"" + chip[4] + "\">\n  </td>\n</tr>";
+    };
+
+    App.prototype.chip_options_html = function(chip) {
+      var chip_id, html, name, _ref;
+      html = "<option value>-</option>";
+      _ref = this.CHIP_IDS;
+      for (chip_id in _ref) {
+        name = _ref[chip_id];
+        html += "<option value='" + chip_id + "' " + (parseInt(chip_id) === parseInt(chip[1]) ? ' selected' : '') + ">" + name + "</option>\n";
+      }
+      return html;
     };
 
     return App;
